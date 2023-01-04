d22fac6119ca5498b8d019ccb73c5c1761fd8c9b
Kerdo Kurs <kerdokurs@gmail.com>
2023-01-04 16:35:06 +0200

    fix casting error

--- permissions.go
+++ permissions.go
@@ -10,14 +10,21 @@ func hasPermissions(token *auth.Token, permissions ...string) bool {
 		return false
 	}
 
-	permissionsMap, ok := claims.(map[string]bool)
+	permissionsMap, ok := claims.(map[string]any)
 	if !ok {
 		return false
 	}
 
 	for _, permission := range permissions {
-		if !permissionsMap[permission] {
+		if value, ok := permissionsMap[permission]; !ok {
+			// Permission is not in the permissions map on the token
 			return false
+		} else {
+			boolValue, ok := value.(bool)
+			if !ok || !boolValue {
+				// Value is not a boolean or is false
+				return false
+			}
 		}
 	}
 
